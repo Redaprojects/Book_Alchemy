736a07540df736bc24b724c99137c985cb64eb77
Red.prjects <red.me1aqw@gmail.com>
2025-06-27 21:08:51 +0200

    Connect flask app with the database

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask
+# SQLAlchemy: ORM we will use to define and manage models.
 from flask_sqlalchemy import SQLAlchemy
+from data_models import db, Author, Book
 
 app = Flask(__name__)
 
+# SQLite connection.
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data/library.sqlite'
 
+# This will connect the Flask app to the flask-sqlalchemy code.
+# Binds database instance to app
+db.init_app(app)
 
